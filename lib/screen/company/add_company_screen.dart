7e5119f88c440be40cf5c095f47501b4ed52b1df
unknown <tritechuzaifa@gmail.com>
2023-07-08 03:44:16 -0700

    hafiz bismil 08/07 3:43 pm

---
+++ lib/screen/company/add_company_screen.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:furniture_admin/static/bio_input_field.dart';
+import 'package:furniture_admin/static/topbar.dart';
+
+class AddCompanyScreen extends StatefulWidget {
+  const AddCompanyScreen({super.key});
+
+  @override
+  State<AddCompanyScreen> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<AddCompanyScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: Container(
+        padding: EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            TopBar(
+              text: ' Add Company',
+              ontap: () {},
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: 104,
+              decoration: ShapeDecoration(
+                color: Color(0x11823E13),
+                shape: RoundedRectangleBorder(
+                  side: BorderSide(width: 1, color: Color(0xFF823E14)),
+                  borderRadius: BorderRadius.circular(15),
+                ),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(bottom: 10),
+                    child: Image(
+                        image: AssetImage('assets/images/addcompanyy.png')),
+                  ),
+                  Text(
+                    'Upload Photo',
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+                  )
+                ],
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Text(
+                  'Bio in english',
+                  style: TextStyle(fontSize: 14, fontWeight: FontWeight.w500),
+                ),
+              ],
+            ),
+            BioInputField(),
+            
+          ],
+        ),
+      )),
+    );
+  }
+}
---
+++ lib/static/bio_input_field.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+
+class BioInputField extends StatelessWidget {
+  const BioInputField(
+      {Key? key,
+      this.controller,
+      this.hint,
+      this.text,
+      this.color = Colors.black,
+      this.obscure = false,
+      this.maxlines = false,
+      this.enabled = true,
+      this.readOnly = false,
+      this.onChange,
+      this.imageIcon,
+      this.validator,
+      this.autovalidateMode,
+      // this.validate,
+      this.icon,
+      this.type = TextInputType.text,
+      this.fontSize = 17.0,
+      this.onpressed})
+      : super(key: key);
+
+  final controller;
+  final validator;
+  // final RxBool? validate;
+  final obscure;
+  final hint;
+  final type;
+  final imageIcon;
+  final icon;
+  final text;
+  final autovalidateMode;
+  final color;
+  final maxlines;
+  final onChange;
+  final onpressed;
+  final fontSize;
+  final enabled;
+  final readOnly;
+
+  @override
+  Widget build(BuildContext context) {
+    // bool isDark = Provider.of<DarkThemeProvider>(context).darkTheme;
+
+    return TextFormField(
+      readOnly: readOnly,
+      enabled: enabled,
+      controller: controller,
+      style: TextStyle(fontSize: fontSize),
+      obscureText: obscure,
+      keyboardType: type,
+      validator: validator,
+      // autovalidateMode: autovalidateMode ??
+      //     (validate == true.obs
+      //         ? AutovalidateMode.always
+      //         : AutovalidateMode.onUserInteraction),
+      decoration: InputDecoration(
+        hintText: hint,
+        hintStyle: TextStyle(color: Colors.grey),
+        fillColor: color,
+        suffixIcon: IconButton(
+          onPressed: onpressed,
+          icon: imageIcon != null
+              ? Padding(
+                  padding: const EdgeInsets.all(4.0),
+                  child: ImageIcon(
+                    AssetImage(imageIcon),
+                    color: Colors.grey,
+                  ),
+                )
+              : Icon(
+                  icon,
+                  color: Colors.grey,
+                  size: 26,
+                ),
+        ),
+        enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey[300]!),
+                        borderRadius: BorderRadius.all(Radius.circular(10))),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey[300]!),
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                    ),
+      ),
+      cursorColor: Colors.black,
+      maxLines: maxlines == true ? null : 1,
+    );
+  }
+}
